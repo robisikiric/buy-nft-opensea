d6a2de188baefb03798ad34a677b7d8e5416618a
robisikiric <robisikiric@gmail.com>
2024-10-23 17:59:55 +0900

    update deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,7 +5,7 @@ async function main() {
     console.log("Deploying contracts with the account:", deployer.address);
   
     const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-    const marketplace = await NFTMarketplace.deploy(); 
+    const marketplace = await NFTMarketplace.deploy("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"); 
   
     console.log("NFTMarketplace deployed to:", marketplace.address);
   }
